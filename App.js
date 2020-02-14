b7bdd68091fbf32389178c95fcdd7efd0c2d4e06
Josue Climaco <hjosue656@gmail.com>
2020-02-13 19:44:23 -0600

    Estudiantes Onlive

--- App.js
+++ App.js
@@ -8,29 +8,27 @@ document.addEventListener("DOMContentLoaded", e=>{
             telefono = document.querySelector("#txtTelefonoAlumno").value;
 
         console.log(codigo, nombre, direccion, telefono);
-        
         if( 'localStorage' in window ){
-            window.localStorage.setItem("codigo", codigo);
-            window.localStorage.setItem("nombre", nombre);
-            window.localStorage.setItem("direccion", direccion);
-            window.localStorage.setItem("telefono", telefono);
+            window.localStorage.setItem("codigo"+codigo, codigo);
+            window.localStorage.setItem("nombre"+codigo, nombre);
+            window.localStorage.setItem("direccion"+codigo, direccion);
+            window.localStorage.setItem("telefono"+codigo, telefono);
         } else {
             alert("Por favor ACTUALIZATE!!!.");
         }
     });
     document.querySelector("#btnRecuperarAlumno").addEventListener("click", event=>{
-        document.querySelector("#txtCodigoAlumno").value = window.localStorage.getItem("codigo");
-        document.querySelector("#txtNombreAlumno").value = window.localStorage.getItem("nombre");
-        document.querySelector("#txtDireccionAlumno").value = window.localStorage.getItem("direccion");
-        document.querySelector("#txtTelefonoAlumno").value = window.localStorage.getItem("telefono");
+
+        if (document.querySelector("#txtCodigoAlumno").value = !""){
+            document.querySelector("#txtCodigoAlumno").value = window.localStorage.getItem("codigo"+document.querySelector("#txtCodigoAlumno").value);
+            document.querySelector("#txtNombreAlumno").value = window.localStorage.getItem("nombre"+document.querySelector("#txtCodigoAlumno").value);
+            document.querySelector("#txtDireccionAlumno").value = window.localStorage.getItem("direccion"+document.querySelector("#txtCodigoAlumno").value);
+            document.querySelector("#txtTelefonoAlumno").value = window.localStorage.getItem("telefono"+document.querySelector("#txtCodigoAlumno").value);
+        }
+        else if (document.querySelector("#txtCodigoAlumno").value = ""){
+            alert("Introduzca un codigo de usuario");
+        }
+
+  
     });
-});
-
-/*document.addEventListener("DOMContentLoaded",function(e){
-    alert("CALLBACK LISTO");
-});*/
-
-/*document.addEventListener("DOMContentLoaded", init);
-function init(e){
-    alert("LISTO");
-}*/
+})
